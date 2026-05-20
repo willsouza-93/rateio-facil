cbcaf1d3c478d2dd6972ae1abf5f78b3043ce985
willsouza-93 <wilkblackvidaloka@gmail.com>
2026-05-19 22:46:55 -0300

    chore(localstorage-and-offline-version): Adicionando recurso para salvar os dados e acesso offline

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "rateio-pwa-v7";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
